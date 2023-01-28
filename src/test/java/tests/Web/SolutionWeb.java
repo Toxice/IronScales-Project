0781a66cef42175d5b22fd3e7f46ba5ab6bafb52
Toxice <38692012+Toxice@users.noreply.github.com>
2023-01-28 02:01:15 +0200

    Update SolutionWeb.java

--- src/test/java/tests/Web/SolutionWeb.java
+++ src/test/java/tests/Web/SolutionWeb.java
@@ -14,16 +14,14 @@ public class SolutionWeb extends CommonOps {
 
     @Test
     public void test01_validate_starter_tab(){
-        WebActions.hoverElements(navigationBar.SolutionTab , navigationBar.PlanTab);
-
-        WebActions.hoverElement(navigationBar.Starter); 
+        
+        WebFlows.getSolutionTab(navigationBar.Starter); 
 
         Verifications.verifyText(navigationBar.Starter , "Starter™");
     }
 
     @Test
     public void test02_validate_email_tab(){
-
 
         WebFlows.getSolutionTab(navigationBar.emailProtect);
 
@@ -32,6 +30,7 @@ public class SolutionWeb extends CommonOps {
 
     @Test
     public void test03_validate_protect_tab() {
+        
         WebFlows.getSolutionTab(navigationBar.completeProtect);
 
         Verifications.verifyText(navigationBar.completeProtect, "Complete Protect™");
