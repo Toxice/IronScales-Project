321188e09776eda45eaf73f80c9a5f32400dcb22
Toxice <38692012+Toxice@users.noreply.github.com>
2023-01-29 06:50:42 +0200

    Update RemoveJava.java
    deleted comments

--- src/test/java/tests/Java/RemoveJava.java
+++ src/test/java/tests/Java/RemoveJava.java
@@ -7,19 +7,16 @@ public class RemoveJava {
     @Test
     public void test01_hello_world(){
         Verifications.verifyRemove(" hello world!  " , "hello world!");
-        // 1 space in beginning, 2 spaces in the end
     }
 
     @Test
     public void test02_IronScales(){
         Verifications.verifyRemove("  Welcome to IronScales  " , "Welcome to IronScales");
-        // 2 spaces in the beginning, 2 spaces in the end
     }
 
     @Test
     public void test03_supposed_to_fail(){
         Verifications.verifyRemove("    This test supposed to fail!    " , "    This test supposed to fail!    ");
-        // the expected string is the same as the actual one, so this test is going to fail (as asked by the job test)
     }
 
 }
